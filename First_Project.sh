65e8a70567c60f175027444037a32dbdfae178f1
Drew Turner <drewturnerengineering@protonmail.com>
2024-11-14 09:38:31 -0500

    Written out the basics of user login and account creation

---
+++ First_Project.sh
@@ -0,0 +1,23 @@
+# This is the game!
+# The game will create a file called Game_Files, if none already exists.
+# It will then ask for login or to create an account.
+# Once it has verified credentials VIA login or created account it will ask to start the game.
+# Once game is started, the game will generate a random letter and ask the user for input of a word (String).
+# It will then check if the string includes the letter of the attempt or not.
+# It will then record the attempt with account, date and time, win or loss, generated letter, and guessed word.
+
+
+# If Game_Files is not in "echo ls" (looking in current directory) touch Game_Files
+
+# Ask user for username or password or if they would like to create an account
+
+# If username and password provided then check login credentials from Game_Files.
+#	If username and password match a record in Game_Files then display "Hello (user). press "s" to start the game!".
+#       If username and password DO NOT match a record in Game_Files then display "Username or password don't exist or match.".
+
+# If user opts to create credentials then ask for username and password and record those in Game_Files.
+# Then echo those credentials to the login algorythm and login.
+
+
+
+
