f86179dcfb5e2c7ee1815848d4130968f97bb48f
Drew Turner <drewturnerengineering@protonmail.com>
2024-11-15 14:38:20 -0500

    added stuff

--- Analyzer.sh
+++ Analyzer.sh
@@ -9,4 +9,21 @@ source ./Library.sh
 #    delimited_Line_Array=$(echo "$line" | awk -F'_' '{print $(NF-1)}')
 # done < Game_Files.txt 
 
-echo "$wins" >> ./Statistics.txt
+# echo "$wins" >> ./Statistics.txt
+
+
+# while IFS= read -r line
+# do
+#    delimited_Line_Array=$(echo "$line" | awk -F'_' '{print $(NF-1)}')
+# done < Game_Files.txt 
+
+function wins(){
+ file=Game_Files.txt
+ WinsAndLosses=()
+ while read -r line; do
+   IFS='_' read -r -a Game_Stats_Array <<< "$line"
+   # Only display wins and losses
+   WinsAndLosses+=$(echo ${Game_Stats_Array[7]})" "
+done < "$file"
+}
+echo "${WinsAndLosses[@]}" >> Statistics.txt
